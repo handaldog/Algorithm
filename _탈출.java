433694ec805406c4cb1dc792e628e7c4ff0b5933
handal <96431408+handaldog@users.noreply.github.com>
2023-04-05 23:38:54 +0900

    Update _탈출.java

--- _탈출.java
+++ _탈출.java
@@ -1,3 +1,5 @@
+package yang;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,39 +10,37 @@ import java.util.StringTokenizer;
 public class _탈출 {
 
 	static class node {
-		int wx;
-		int wy;
-		int sx;
-		int sy;
+		int x;
+		int y;
+		int time;
 
-		node(int wx, int wy, int sx, int sy) {
-			this.wx = wx;
-			this.wy = wy;
-			this.sx = sx;
-			this.sy = sy;
+		node(int x, int y, int time) {
+			this.x = x;
+			this.y = y;
+			this.time = time;
 		}
 	}
 
-	static int Di, Dj;
+	static int dociX, dociY, caveX, caveY, waterX, waterY, r, c;
 	static int di[] = { 1, -1, 0, 0 };
 	static int dj[] = { 0, 0, 1, -1 };
-	static Queue<node> que = new LinkedList<>();
+	static Queue<node> water = new LinkedList<>();
+	static Queue<node> dochi = new LinkedList<>();
+	static boolean wvisit[][];
+	static boolean dovisit[][];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
 
 		char arr[][] = new char[r][c];
-		Di = 0;
-		Dj = 0;
-		int Si = 0;
-		int Sj = 0;
-		int seai = 0;
-		int seaj = 0;
+
+		wvisit = new boolean[r][c];
+		dovisit = new boolean[r][c];
 
 		for (int i = 0; i < r; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -48,35 +48,58 @@ public class _탈출 {
 			for (int j = 0; j < c; j++) {
 				arr[i][j] = str.charAt(j);
 				if (arr[i][j] == 'D') {
-					Di = i;
-					Dj = j;
+					caveX = i;
+					caveY = j;
 				}
 				if (arr[i][j] == 'S') {
-					Si = i;
-					Sj = j;
+					dociX = i;
+					dociY = j;
 				}
 				if (arr[i][j] == '*') {
-					seai = i;
-					seaj = j;
+					waterX = i;
+					waterY = j;
 				}
 			}
 		}
+		water.offer(new node(waterX, waterY, 1));
+		dochi.offer(new node(dociX, dociY, 1));
 
 	}
 
-	public static void bfs(int si, int sj, int seai, int seaj, int cnt) {
-		
-		while(!que.isEmpty()) {
-			node no = que.poll().
-		}
-		
-		if (si == Di && sj == Dj) {
-			return;
-		}
+	public static void bfs() {
 
-		for (int c = 0; c < 4; c++) {
-			int Nsi = si + di[c];
-			int Nsj = sj + dj[c];
+		while (!water.isEmpty()) {
+			node wno = water.poll();
+
+			wvisit[wno.x][wno.y] = true;
+
+			for (int c = 0; c < 4; c++) {
+				int wnexti = wno.x + di[c];
+				int wnextj = wno.y + dj[c];
+
+				if (wnexti >= 0 && wnextj >= 0 && wnexti < r && wnextj < c && !wvisit[wnexti][wnextj]) {
+					wvisit[wnexti][wnextj] = true;
+					water.offer(new node(wnexti, wnextj, wno.time + 1));
+				}
+			} // water 4방면 끝나는 부분
+
+			while (!dochi.isEmpty()) {
+				node dno = dochi.poll();
+
+				wvisit[dno.x][dno.y] = true;
+
+				for (int c = 0; c < 4; c++) {
+					int dnexti = dno.x + di[c];
+					int dnextj = dno.y + dj[c];
+
+					if (dnexti >= 0 && dnextj >= 0 && dnexti < r && dnextj < c && !dovisit[dnexti][dnextj] && !wvisit[dnexti][dnextj]) {
+						dovisit[dnexti][dnextj] = true;
+						dochi.offer(new node(dnexti, dnextj, dno.time + 1));
+					}
+				}
+
+			}
+
 		}
 
 	}
